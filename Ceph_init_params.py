a212e9565c67af8b284f6e0a52913cc2225728b2
aawallace <64704748+aawallace@users.noreply.github.com>
2020-10-24 13:49:02 -0500

    added citations
    I added citations for the two equations which I obtained via literature search

--- Ceph_init_params.py
+++ Ceph_init_params.py
@@ -16,14 +16,14 @@ X = float(1 - (Y + Z))
 
 # original M-L relationship: L = float(10**(0.72 + 3.35*np.log10(M) + 1.36*np.log10(Y/0.28) - 0.34*np.log(Z/0.02)))
 
-M = float(10**((1/3.35)*(np.log10(L) - 0.72 - 1.36*np.log10(Y/0.28) + 0.34*np.log10(Z/0.02))))
+M = float(10**((1/3.35)*(np.log10(L) - 0.72 - 1.36*np.log10(Y/0.28) + 0.34*np.log10(Z/0.02)))) #citation [1]
 
 #original data: Teff_theoretical = float(-0.047135*L + 5915.741956)
 # original data: sigma = 449.98
 
 R_sol = float(696340000) #radius of sun in meters
 
-R = R_sol*10**( (np.log10(M) + (np.random.normal(0.09, 0.03))) / (np.random.normal(0.48, 0.03)) )
+R = R_sol*10**( (np.log10(M) + (np.random.normal(0.09, 0.03))) / (np.random.normal(0.48, 0.03)) ) #citation [2]
 
 sigma = 5.670374419*10**-8 #Joules*meters**-2*s**-1*K**-4
 
@@ -41,3 +41,6 @@ print("L =", L )
 print( "T_eff =", Teff_actual ) 
 print( "X =", X )
 print( "Z =", Z )
+
+#[1]- Caputo et al. “Pulsation and evolutionary masses of classical Cepheids. I. Milky Way variables.” arXiv:astro-ph/0505149
+#[2]- Bono et al. “Improving the mass determination of Galactic Cepheids.” arXiv:astro-ph/0108271
