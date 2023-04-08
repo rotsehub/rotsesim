22c63dd67cf36d6c8351642912c826e3ba669b86
george_pantelimon <80425896+gigicu01@users.noreply.github.com>
2023-04-08 13:24:54 -0400

    Added documentation to StarGenerator

--- py/rotsesim/cepheid/vstarsim/Scheduling/StarGenerator.py
+++ py/rotsesim/cepheid/vstarsim/Scheduling/StarGenerator.py
@@ -13,12 +13,43 @@ import os
 
 class StarGenerator:
     def GenerateStars(field_center_ra: float, field_center_dec: float, fov: float, min_star_separation: float, excel_files_location: str, instaSolarLuminosity: float):
+        """
+            This functions is getting all csv files in a folder specified in the config file and then for each excel file (one excel file = one star)
+            it reads the data, assigns coordinates to the star in the field of view and then creats a star object that is added to the array it returns.
+            
+            Parameters
+            ----------
+            field_center_ra : float
+                The RA of the field center in degrees. This value comes from the config file
+                
+            field_center_dec: float
+                The DEC of the field center in degrees. This value comes from the config file
+                
+            fov: float
+                The size of the fov in degrees. This value comes from the config file
+            
+            min_star_separation: float
+                The minimum separation between stars in degrees. This value comes from the config file
+                
+            excel_files_location: str
+                Location of the excel files that contains the simulated data. This value comes from the config file
+                
+            instaSolarLuminosity: float
+                A constant that represents the instant solar luminosity. This is used to convert the luminosity given my MESA to solar units.
+            
+            Returns
+            -------
+            Array of stars
+                An array of star objects that contain the simulated data and the generated RA and DEC coordinates of the star.
+            
+        """
         stars = []
-        for filename in os.listdir(excel_files_location):
+    
+        for filename in os.listdir(excel_files_location): # go through each csv file (star)
             overlap = True
-            f = os.path.join(excel_files_location, filename)
+            f = os.path.join(excel_files_location, filename) # f is the full path that refers to the current file we are looking at
 
-            df = pd.read_csv(f, header = 0, low_memory=False)
+            df = pd.read_csv(f, header = 0, low_memory=False) # read the data from the csv
             df['luminosity'] = df['luminosity'] * instaSolarLuminosity ## convert the luminosity column to solar units
             df = df.dropna() ## remove empty rows
             while overlap == True: ## generate new coordinates until they don't overalp with other stars
@@ -45,6 +76,19 @@ class StarGenerator:
         return stars
     
     def plot_stars(stars: list[Star]):
+        """
+            This functions is getting all csv files in a folder specified in the config file and then for each excel file (one excel file = one star)
+            it reads the data, assigns coordinates to the star in the field of view and then creats a star object that is added to the array it returns.
+            
+            Parameters
+            ----------
+            stars: list[Star] 
+                A list containing all star objects that were generated (the number should be equal to the number of inpu csv files).   
+            
+            Returns
+            -------
+                Nothing. It will jost plot the stars based on their RA and DEC coordinates in the simulated fov.         
+        """
         ra = [star.getRA() for star in stars]
         dec = [star.getDec() for star in stars]
         plt.scatter(ra, dec)
